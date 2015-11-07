8c8b15c9c1b0c2f1898a10bfb242540bceec6d89
EmilienDupont <emilien.dupont@ymail.com>
2015-11-07 12:53:46 -0800

    added descriptions and renamed variables

--- similarity.m
+++ similarity.m
@@ -1,13 +1,32 @@
 load UserSongSparseMatrix100000.txt
 
-% R = rating matrix
-R = spconvert(UserSongSparseMatrix100000);
+% Count = count matrix
+% Row: User
+% Column: Song
+% Entries: SongCount
+% Size: numUsers * numSongs
+Count = spconvert(UserSongSparseMatrix100000);
 
-% R_max = column vector with max song count for each user
-R_max = diag(max(R,[],2).^-1);
+% InvMaxSongCount = diagonal matrix with inverse of max song count for each user
+% Size: numUsers * numUsers
+InvMaxSongCount = diag(max(Count,[],2).^-1);
 
-% D = normalization matrix
-D = diag(sqrt(sum((R).^2,2)).^-1); 
+% R = rating matrix.
+% For each user (row), we divide the row by the highest song
+% count. Therefore, instead of song counts, we have
+% a number between 0 and 1, which we use as a rating.
+% Row: User
+% Column: Song
+% Entries: SongCount/max(SongCount for each user)
+% Size: numUsers * numSongs
+Rating = InvMaxSongCount*Count;
 
-% S = cosine similarity matrix
-S = (R_max*D*R)*(R_max*D*R)';
+% UserNormalize = diagonal user normalization matrix
+% Entries: Inverse of norm of row for each user
+% Size: numUsers * numUsers
+UserNormalize = diag(sqrt(sum((Rating).^2,2)).^-1); 
+
+% CosineUser = user-user cosine similarity matrix
+% Entries (i,j): cosine similarity between user i and j
+% Size: numUsers * numUsers
+CosineUser = (UserNormalize*Rating)*(UserNormalize*Rating)';
