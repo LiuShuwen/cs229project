cbe2cece71d445dc5335cbd1e35ca437951c6c51
William Zhang <wzhang4@stanford.edu>
2015-12-03 15:28:55 -0800

    initialized P and Q to SVD results

--- latentFactorModel.py
+++ latentFactorModel.py
@@ -1,6 +1,7 @@
 import data
 import numpy as np
 import scipy.sparse
+import scipy.sparse.linalg
 import sys
 import time
 
@@ -40,37 +41,42 @@ etaP = .05 # learning rate for P
 lambdaQ = 3 # regularization for Q
 lambdaP = 3 # regularization for P
 
-Q = np.random.rand(ratings.numSongs, k)
-P = np.random.rand(ratings.numUsers, k)
+# initializing the matrices P and Q using the SVD with k largest singular values
+Q, s, vt = scipy.sparse.linalg.svds(ratings.R, k)
 
-numMaxIters = 10
+P = np.dot(np.diag(s), vt).transpose()
 
-print "Training Latent Factor Model..."
+#Q = np.random.rand(ratings.numSongs, k) # u x k
+#P = np.random.rand(ratings.numUsers, k) # i x k
 
-def getObjective():
-    #print ratings.R.shape, Q.shape, P.transpose().shape
-    #objective = np.linalg.norm(ratings.R - np.dot(Q*P.transpose()))**2
-    objective = 0
-    for index in range(ratings.numNonZeros):
-        user = ratings.rows[index]
-        item = ratings.columns[index]
-        rating = ratings.entries[index]
-        objective += (rating - np.dot(Q[item,:], P[user,:]))**2
-    objective += lambdaQ*np.linalg.norm(Q)**2 + lambdaP*np.linalg.norm(P)**2
-    return objective
-
-
-for iteration in range(numMaxIters):
-    print "Iteration", iteration
-
-    for index in range(ratings.numNonZeros):
-        user = ratings.rows[index]
-        item = ratings.columns[index]
-        # rating = R[user,item] does not work
-        rating = ratings.entries[index]
-        epsilon = 2*(rating - np.dot(Q[item,:], P[user,:]))
-        Q[item,:] = Q[item,:] + etaQ*(epsilon*P[user,:] - lambdaQ*Q[item,:])
-        P[user,:] = P[user,:] + etaP*(epsilon*Q[item,:] - lambdaP*P[user,:])
-        # Need a check for convergence
-
-    print "Objective", getObjective()
+#numMaxIters = 10
+#
+#print "Training Latent Factor Model..."
+#
+#def getObjective():
+#    #print ratings.R.shape, Q.shape, P.transpose().shape
+#    #objective = np.linalg.norm(ratings.R - np.dot(Q*P.transpose()))**2
+#    objective = 0
+#    for index in range(ratings.numNonZeros):
+#        user = ratings.rows[index]
+#        item = ratings.columns[index]
+#        rating = ratings.entries[index]
+#        objective += (rating - np.dot(Q[user,:], P[item,:]))**2
+#    objective += lambdaQ*np.linalg.norm(Q)**2 + lambdaP*np.linalg.norm(P)**2
+#    return objective
+#
+#
+#for iteration in range(numMaxIters):
+#    print "Iteration", iteration
+#
+#    for index in range(ratings.numNonZeros):
+#        user = ratings.rows[index]
+#        item = ratings.columns[index]
+#        # rating = R[user,item] does not work
+#        rating = ratings.entries[index]
+#        epsilon = 2*(rating - np.dot(Q[user,:], P[item,:]))
+#        Q[user,:] = Q[user,:] + etaQ*(epsilon*P[item,:] - lambdaQ*Q[user,:])
+#        P[item,:] = P[item,:] + etaP*(epsilon*Q[user,:] - lambdaP*P[item,:])
+#        # Need a check for convergence
+#
+#    print "Objective", getObjective()
